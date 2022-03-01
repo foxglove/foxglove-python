9acb220a236ceba0a04530ad289223a0436d3fdd
foxymiles <93935560+foxymiles@users.noreply.github.com>
2022-03-01 09:27:22 -0700

    Expand on code examples (#3)

---
+++ examples/download.py
@@ -0,0 +1,22 @@
+import datetime
+import sys
+from datetime import datetime, timedelta
+
+from foxglove.data_platform.client import Client
+
+token = "<YOUR API TOKEN HERE>"
+device_id = "<YOUR DEVICE ID>"
+client = Client(token=token)
+
+download_stream = client.download_data(
+    device_id=device_id,
+    start=datetime.now() - timedelta(hours=3),
+    end=datetime.now() - timedelta(hours=1),
+)
+
+data = bytes()
+for chunk in download_stream.iter_content(chunk_size=64 * 1024):
+    sys.stdout.write(".")
+    data += chunk
+
+print("download", len(data))

94036350fb58ac898237d2249588e162392cab86
foxymiles <93935560+foxymiles@users.noreply.github.com>
2022-03-22 07:19:52 -0600

    Add tests & format json for topics & coverage endpoints (#15)

--- tests/test_client.py
+++ tests/test_client.py
@@ -9,27 +9,41 @@ fake = Faker()
 
 
 @responses.activate
-def test_get_events():
-    device_id = "my_device_id"
+def test_download():
+    download_link = fake.url()
+    responses.add(
+        responses.POST,
+        "https://api.foxglove.dev/v1/data/stream",
+        json={
+            "link": download_link,
+        },
+    )
+    data = fake.binary(4096)
+    responses.add(responses.GET, download_link, body=data)
+    client = Client("test")
+    response_data = client.download_data(
+        device_id="test_id", start=datetime.now(), end=datetime.now()
+    )
+    assert data == response_data
+
+
+@responses.activate
+def test_get_coverage():
     responses.add(
         responses.GET,
-        f"https://api.foxglove.dev/beta/device-events?deviceId={device_id}",
+        "https://api.foxglove.dev/v1/data/coverage",
         json=[
             {
-                "id": "1",
-                "createdAt": datetime.now().isoformat(),
-                "deviceId": device_id,
-                "durationNanos": fake.pyint(),
-                "metadata": {},
-                "timestampNanos": fake.pyint(),
-                "updatedAt": datetime.now().isoformat(),
+                "deviceId": "device_id",
+                "start": datetime.now().isoformat(),
+                "end": datetime.now().isoformat(),
             }
         ],
     )
     client = Client("test")
-    events = client.get_events(device_id=device_id)
-    assert len(events) == 1
-    assert events[0]["device_id"] == device_id
+    coverage_response = client.get_coverage(start=datetime.now(), end=datetime.now())
+    assert len(coverage_response) == 1
+    assert coverage_response[0]["device_id"] == "device_id"
 
 
 @responses.activate
@@ -55,41 +69,50 @@ def test_get_devices():
 
 
 @responses.activate
-def test_download():
-    download_link = fake.url()
+def test_get_events():
+    device_id = "my_device_id"
     responses.add(
-        responses.POST,
-        "https://api.foxglove.dev/v1/data/stream",
-        json={
-            "link": download_link,
-        },
+        responses.GET,
+        f"https://api.foxglove.dev/beta/device-events?deviceId={device_id}",
+        json=[
+            {
+                "id": "1",
+                "createdAt": datetime.now().isoformat(),
+                "deviceId": device_id,
+                "durationNanos": fake.pyint(),
+                "metadata": {},
+                "timestampNanos": fake.pyint(),
+                "updatedAt": datetime.now().isoformat(),
+            }
+        ],
     )
-    data = fake.binary(4096)
-    responses.add(responses.GET, download_link, body=data)
     client = Client("test")
-    response_data = client.download_data(
-        device_id="test_id", start=datetime.now(), end=datetime.now()
-    )
-    assert data == response_data
+    events = client.get_events(device_id=device_id)
+    assert len(events) == 1
+    assert events[0]["device_id"] == device_id
 
 
 @responses.activate
-def test_upload():
-    upload_link = fake.url()
+def test_get_topics():
     responses.add(
-        responses.POST,
-        "https://api.foxglove.dev/v1/data/upload",
-        json={
-            "link": upload_link,
-        },
+        responses.GET,
+        "https://api.foxglove.dev/v1/data/topics",
+        json=[
+            {
+                "topic": "/topic",
+                "version": "1",
+                "encoding": "ros1",
+                "schemaEncoding": "ros1msg",
+                "schemaName": "std_msgs/String",
+            }
+        ],
     )
-    responses.add(responses.PUT, upload_link)
     client = Client("test")
-    data = fake.binary(4096)
-    upload_response = client.upload_data(
-        device_id="test_device_id", filename="test_file.mcap", data=data
+    topics_response = client.get_topics(
+        device_id="device", start=datetime.now(), end=datetime.now()
     )
-    assert upload_response["link"] == upload_link
+    assert len(topics_response) == 1
+    assert topics_response[0]["topic"] == "/topic"
 
 
 @responses.activate
@@ -113,5 +136,24 @@ def test_streaming_upload():
         filename="test_file.mcap",
         data=file,
         callback=lambda size, progress: print(".", end=""),
+    )
+    assert upload_response["link"] == upload_link
+
+
+@responses.activate
+def test_upload():
+    upload_link = fake.url()
+    responses.add(
+        responses.POST,
+        "https://api.foxglove.dev/v1/data/upload",
+        json={
+            "link": upload_link,
+        },
+    )
+    responses.add(responses.PUT, upload_link)
+    client = Client("test")
+    data = fake.binary(4096)
+    upload_response = client.upload_data(
+        device_id="test_device_id", filename="test_file.mcap", data=data
     )
     assert upload_response["link"] == upload_link

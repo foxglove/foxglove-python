61c556d6bead11857bd128f9767bb683cf0b798c
Bryan Fox <bryan@foxglove.dev>
2023-04-19 09:10:51 -0400

    Update events example

--- examples/events.py
+++ examples/events.py
@@ -6,13 +6,15 @@ token = "<YOUR API TOKEN>"
 device_id = "<YOUR DEVICE ID>"
 client = Client(token=token)
 
+event_time = datetime.now()
+
 event = client.create_event(
     device_id=device_id,
-    time=datetime.now(),
-    duration=0,
+    start=event_time,
+    end=event_time,
     metadata={"message": "Hi from python!"},
 )
 print(event)
 
-events = client.get_events()
+events = client.get_events(device_id=device_id)
 print(events)
